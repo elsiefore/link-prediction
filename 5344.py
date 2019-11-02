279785c8ffd5d0e5e1102334bc8aeaa6375ace9d
elsiefore <a0119428@u.nus.edu>
2019-11-02 19:23:59 +0800

    add als rmse and large sample

--- 5344.py
+++ 5344.py
@@ -1,7 +1,10 @@
 from pyspark import SparkConf, SparkContext
+from pyspark.sql import SQLContext
 from pyspark.ml.recommendation import ALS
 from pyspark.sql.types import StructType, StructField, IntegerType
 from pyspark.sql.functions import lit
+from pyspark.ml.evaluation import RegressionEvaluator
+
 import re
 import math
 
@@ -28,7 +31,7 @@ def mapLineToUserPairs(line):
     pairs = re.split(' ', line)
     return (pairs[0], pairs[1])
 
-def getDataframeForALS(rdd):
+def getDataframeForALS(rdd, sc):
     rdd_new = rdd.flatMapValues(lambda x: x).map(lambda row: [int(x) for x in row])
     schema = StructType([
         StructField("user", IntegerType(), True),
@@ -37,21 +40,30 @@ def getDataframeForALS(rdd):
     df = df.withColumn("rating", lit(1))
     return df
 
+
 # initialize spark
 conf = SparkConf()
 sc = SparkContext(conf=conf)
+sqlContext = SQLContext(sc)
 
 # read raw data
-raw = sc.textFile("data/links.csv").filter(lambda l: l).map(lambda l: mapLineToUserPairs(l))
+raw = sc.textFile("data/sample.csv").filter(lambda l: l).map(lambda l: mapLineToUserPairs(l))
 pruned_data = pruneData(raw, 4)
 (train_rdd, test_rdd) = trainTestSplit(pruned_data)
 
 
-train_df = getDataframeForALS(train_rdd, sc)
-test_df = getDataframeForALS(test_rdd, sc)
+train_df = getDataframeForALS(train_rdd, sqlContext)
+test_df = getDataframeForALS(test_rdd, sqlContext)
 
 als = ALS(rank=15, maxIter=10, regParam=0.01, userCol="user", itemCol="item", ratingCol='rating',
           coldStartStrategy="drop")
 als_model = als.fit(train_df)
+
+# evaluate the performance of ALS
+als_predictions = als_model.transform(test_df)
+als_evaluator = RegressionEvaluator(metricName="rmse", labelCol="rating",
+                                predictionCol="prediction")
+als_auc = als_evaluator.evaluate(als_predictions)
+print("Root-mean-square error = " + str(als_auc))
 # Top 10 recommendations per user
 userRecs = als_model.recommendForAllUsers(10) 
